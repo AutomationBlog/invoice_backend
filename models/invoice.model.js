c08b01cd3ed35e843664bade4a4dec29b62c680d
automationblog <sawin24@gmail.com>
2024-11-01 21:31:55 +0530

    date updated

--- models/invoice.model.js
+++ models/invoice.model.js
@@ -1,38 +1,33 @@
 import mongoose from "mongoose";
 
-const invoiceSchema = new mongoose.Schema(
-  {
-    id: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      required: true,
-      default: Timestamp.now,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-      enum: ["pending", "paid"],
-      default: "pending",
-    },
+const invoiceSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["pending", "paid"],
+    default: "pending",
+  },
+});
 
 export const Invoice = mongoose.model("Invoice", invoiceSchema, "invoice");
